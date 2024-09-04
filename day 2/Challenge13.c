3eddc12dc8fbec1c9233e89edf9060c180833972
safaa <safaeettalhi@gmail.com>
2024-09-04 12:22:28 +0100

    ajouter le challege 13

---
+++ day 2/Challenge13.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main() {
+    int n,i;
+    printf("entrer la taille de tableau");
+    scanf("%d",&n);
+    int tableau[n];
+    
+    for (int i = 0; i < n; i++) {
+        printf("Entrer la valeur: %d\n", i+1);
+        scanf("%d", &tableau[i]);
+    }
+    for(i=0;i<n;i++){
+        if(tableau[i]%2!=0){
+         printf("%d ", tableau[i]);
+         
+        }
+    }
+     printf("\n");
+    
+    return 0;
+}
