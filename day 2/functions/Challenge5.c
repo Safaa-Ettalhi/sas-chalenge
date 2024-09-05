e0768a5732356a3618c094bbe32836efaeefd2c9
safaa <safaeettalhi@gmail.com>
2024-09-05 12:37:47 +0100

    add challenge 5  function factoriel

---
+++ day 2/functions/Challenge5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int factorielle(int a){
+  unsigned int result = 1;
+    for (int i = 1; i <= a; i++) {
+        result *= i;
+    }
+    return result;
+}
+int main() {
+    int a;
+    printf("Entrez un entier : ");
+    scanf("%d", &a);
+    if (a < 0) {
+        printf(" les entiers positifs doivent etre positif.\n");
+    } else {
+        
+        printf("La factorielle de %d est %d\n", a, factorielle(a));
+    }
+
+    return 0;
+}
