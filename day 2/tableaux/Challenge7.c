efb9a0b803ad97f6d54a2fa4429c87ddaf179a2f
safaa <safaeettalhi@gmail.com>
2024-09-06 10:24:13 +0100

    modif

--- day 2/tableaux/Challenge7.c
+++ day 2/tableaux/Challenge7.c
@@ -16,11 +16,11 @@ int main() {
         scanf("%d", &tableau[i]);
     }
     for (int i = 0; i < n-1; i++) {
-        for (int j = 0; j < n-i-1; j++) {
+        for (int j = 0; j < n-1; j++) {
             if (tableau[j] > tableau[j+1]) {
                 int temp = tableau[j];
                 tableau[j] = tableau[j+1];
-                tableau[j+1] = temp;
+                tableau[j+1] = temp;        
             }
         }
     }
