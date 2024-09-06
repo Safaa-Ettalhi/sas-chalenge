c9496bd3e736b6cafdc7787fb86109218d3c7067
safaa <safaeettalhi@gmail.com>
2024-09-06 12:57:04 +0100

    Trouver un Sous-String

---
+++ day 3/string/Challenge10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+
+// un programme C qui lit une chaîne de caractères et supprime tous les espaces
+
+int main() {
+    char caract1[50];
+    char sous_chaine[20];
+    printf("entrer une chaîne de caracteres");
+    scanf("%[^\n]",&caract1);
+    getchar();
+    printf("entrer une une sous-chaine");
+    scanf("%[^\n]",&sous_chaine);
+    getchar();
+   if (strstr(caract1, sous_chaine) != NULL) {
+        printf("La sous-chaîne \"%s\" est présente dans la chaîne principale.\n", sous_chaine);
+    } else {
+        printf("La sous-chaîne \"%s\" n'est pas présente dans la chaîne principale.\n", sous_chaine);
+    }
+   
+    return 0;
+}
