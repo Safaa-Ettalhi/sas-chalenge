7a4ef3b566b000405cac6fb864b8987e75c50e4d
safaa <safaeettalhi@gmail.com>
2024-09-09 18:28:30 +0100

    Ajouter la fonction afficher tous les taches selon ordre alpha ou deadline

--- to do liste projets/mini_projet.c
+++ to do liste projets/mini_projet.c
@@ -72,6 +72,50 @@ void ajouterpleusieur(){
         }
         }    
 }
+void AfficherListe() {
+    int choix;
+
+    printf("Choisissez l'option convenable\n");
+    printf("1. Afficher la liste de toutes les tâches.\n");
+    printf("2. Afficher la liste de toutes les tâches par deadline.\n");
+    printf("3. Afficher la liste de toutes les tâches par ordre alphabétique.\n");
+
+    printf("Entrer votre choix: ");
+    scanf("%d", &choix);
+    getchar();
+
+    printf("Liste des tâches:\n");
+
+    switch (choix) {
+        case 1:
+            for (int i = 0; i < nb; i++) 
+                printf("Tache %d\n\tID: %s | Titre: %s | Description: %s | Deadline: %s | Statut: %s\n", i+1, tache[i].ID, tache[i].Titre, tache[i].Desc, tache[i].Deadline, tache[i].Statut);
+            break;
+
+        case 2:
+            for (int i = 0; i < nb - 1; i++) {
+                for (int j = 0; j < nb - i - 1; j++) {
+                    if (strcmp(tache[j].Deadline, tache[j + 1].Deadline) > 0) {
+                        Todo tmp = tache[j];
+                        tache[j] = tache[j + 1];
+                        tache[j + 1] = tmp;
+                    }
+                }
+            }
+            break;
+
+        case 3:
+            for (int i = 0; i < nb-1; i++) {
+                for (int j = 0; j < nb-i-1; j++) {
+                    if (strcmp(tache[j].Titre, tache[j+1].Titre) > 0) {
+                        Todo tmp = tache[j];
+                        tache[j] = tache[j+1];
+                        tache[j+1] = tmp;
+                    }
+                }
+            }
+    } 
+}
 void ModifierTache() {
     char idnew[30], descnew[200], statutnew[30], deadlinenew[30];
     int choix;
